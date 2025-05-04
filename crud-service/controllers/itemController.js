86f97fca8c117c50f1e4ed5aa630ab611f7a4d9f
bappadeydevelopment <bappa.development@gmail.com>
2025-05-04 21:39:21 +0530

    [WORKING...]

--- crud-service/controllers/itemController.js
+++ crud-service/controllers/itemController.js
@@ -1,10 +1,19 @@
 const Item = require('../models/Item');
+const Redis = require('ioredis');
+const redis = new Redis(process.env.REDIS_URL);
 
 exports.createItem = async (req, res) => {
   const { name, description } = req.body;
   try {
     const item = await Item.create({ name, description, createdBy: req.user.id });
     res.status(201).json(item);
+
+    // Added notification service
+    await redis.publish('item-events', JSON.stringify({
+      type: 'item_created',
+      name: newItem.name,
+      userId: req.user.id
+    }));
   } catch (err) {
     res.status(500).json({ message: 'Create failed' });
   }
@@ -35,4 +44,11 @@ exports.deleteItem = async (req, res) => {
   const { id } = req.params;
   await Item.findOneAndDelete({ _id: id, createdBy: req.user.id });
   res.json({ message: 'Deleted' });
+
+  // Added notification services
+  await redis.publish('item-events', JSON.stringify({
+    type: 'item_deleted',
+    itemId: req.params.id,
+    userId: req.user.id
+  }));
 };
